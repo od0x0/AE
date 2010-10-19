0c3afb5ae412d152bdce7f8790b6fd92a1197a3c
Oliver Daids <oliver@ep-7.net>
2010-10-19 19:21:23 -0400

    OK TEH1GHOOL I FIXED IT.

--- Examples/03-RenderLoops-SDL.c
+++ Examples/03-RenderLoops-SDL.c
@@ -5,7 +5,7 @@ void FrameUpdate(AEContext* context, double timeSinceLastCall, void* arg){
 	puts("I'm supposed to be rendering and handling input, but I decided to SPAMYOURCONSOLEINSTEAD.");
 }
 
-void FrameUpdate(AEContext* context, double timeSinceLastCall, void* arg){
+void FixedUpdate(AEContext* context, double timeSinceLastCall, void* arg){
 	puts("I'm supposed to be doing physics right now, but I decided to SPAMYOURCONSOLEINSTEAD.");
 }
 
@@ -13,10 +13,10 @@ int main(int argc, char** argv){
 	//AEContextActiveGet()->inFullscreen=true;
 	AEContextInit(NULL, "A Simple Window", 800, 500);
 	//This is used to set the specified callback, the last argument is the last arg passed back as the last argument of the callback
-	AEContextCallbackSet(NULL, AEContextCallbackFrameUpdate, FrameUpdate, NULL);
-	
+	AEContextCallbackSet(NULL, AEContextCallbackFrameUpdate, NULL, NULL);
+	//FrameUpdate
 	//The max should be divisible by the min
-	AEContextActiveGet()->fixedUpdateFrameRateMax=30;
+	AEContextActiveGet()->fixedUpdateFrameRateMax=60;
 	AEContextActiveGet()->fixedUpdateFrameRateMin=15;
 	//Same function signature as frameupdate
 	AEContextCallbackSet(NULL, AEContextCallbackFixedUpdate, FixedUpdate, NULL);
